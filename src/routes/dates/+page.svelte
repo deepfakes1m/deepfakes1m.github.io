92bb1adeaa836df41e5de1b94d8e4593072795e5
ControlNet <smczx@hotmail.com>
2024-06-19 19:19:12 +1000

    Update +page.svelte

--- src/routes/dates/+page.svelte
+++ src/routes/dates/+page.svelte
@@ -6,8 +6,8 @@
         {name: "Train and validation set release", date: "March 9, 2024"},
         {name: "Test set release & Evaluation begin", date: "May 15, 2024"},
         {name: "Evaluation end", date: "June 7, 2024"},
-        {name: "Paper Submission Deadline", date: "June 19, 2024"},
-        {name: "Final Decision", date: "July 15, 2024"},
+        {name: "Paper Submission Deadline", date: "TBD"},
+        {name: "Final Decision", date: "TBD"},
         {name: "Camera Ready Deadline", date: "July 29, 2024"},
     ]
 
