fc56343d8360378744cc560c9813add223dc986d
ControlNet <smczx@hotmail.com>
2024-07-29 02:52:56 +1000

    Update +page.svelte

--- src/routes/dates/+page.svelte
+++ src/routes/dates/+page.svelte
@@ -8,7 +8,8 @@
         {name: "Evaluation end", date: "June 7, 2024"},
         {name: "Paper Invitation Decision", date: "July 1, 2024"},
         {name: "Paper Submission Deadline", date: "July 26, 2024"},
-        {name: "Camera Ready Deadline", date: "August 10, 2024"},
+        {name: "Final Decision", date: "July 31, 2024"},
+        {name: "Camera Ready Deadline", date: "August 7, 2024"},
     ]
 
     const currentIndex = 4;
