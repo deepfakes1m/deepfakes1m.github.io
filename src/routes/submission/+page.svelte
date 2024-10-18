941084f2621eeadcc63558850639ec3cc4850327
ControlNet <smczx@hotmail.com>
2024-10-18 12:41:33 +1100

    add benchmark link

--- src/routes/submission/+page.svelte
+++ src/routes/submission/+page.svelte
@@ -13,7 +13,7 @@
 <div class="flex flex-col min-h-screen">
 
     <div class="h-16 w-full"/>
-    <h1 class="text-4xl font-bold text-center">Submission</h1>
+    <h1 class="text-4xl font-bold text-center">Evaluation</h1>
 
     <br>
 
@@ -23,11 +23,41 @@
             <div class="card w-full bg-base-200 shadow-xl">
                 <div class="card-body">
                     <h2 class="card-title">Benchmark Platform</h2>
-                    <p>Competition is ended.
+                    <p>We use Codabench (<a href="https://www.codabench.org/competitions/4221/?secret_key=b8639f4e-8227-4018-93f3-f7d8fff6b0e1" class="link link-info">link</a>)
+                    for evaluation the test set.
                     </p>
                 </div>
             </div>
 
+            <br>
+
+            <div class="card w-full bg-base-200 shadow-xl">
+                <div class="card-body">
+                    <h2 class="card-title">Submission Steps</h2>
+                    <h4 class="font-bold">Step 1 - Benchmark platform:</h4>
+                    <p>Sign up your account in Codabench and participate the benchmark (please note all participants should submit the form and send us the signed
+                        EULA in <a href="{base}/registration" class="link link-info"
+                            on:click={() => {
+                            tabNum.set(1)
+                            goto('/registration')
+                        }}>registration</a> to get the dataset).</p>
+
+                    <br>
+                    <h4 class="font-bold">Step 2 - Prepare predictions:</h4>
+                    <p>Prepare your submission files with the format mentioned in <a href="{base}/details" class="link link-info" on:click={() => {
+                            tabNum.set(2)
+                            goto('/details')
+                        }}>details</a>. Name your prediction files for <span class="italic">Task 1</span> as "prediction.txt",
+                        prediction file for <span class="italic">Task 2</span> as "prediction.json". You can include either or both
+                        of them based on the tasks you participate, <span class="font-bold text-primary">then compress them as a zip file</span>.
+                    </p>
+
+                    <br>
+                    <h4 class="font-bold">Step 3 - Submission:</h4>
+                    <p>In Codabench tab "My Submissions", section "Submission upload", <span
+                            class="font-bold text-primary">select the tasks</span>, then upload the zip file.</p>
+                </div>
+            </div>
         </div>
     </div>
     <div class="h-12 footer-pad"/>
