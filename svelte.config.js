0d8495db08e0ab5effe6a1ffc251acc1ad8a095d
ControlNet <smczx@hotmail.com>
2025-03-02 00:57:40 +1100

    try to fix 404

--- svelte.config.js
+++ svelte.config.js
@@ -15,9 +15,9 @@ const config = {
             fallback: "404.html"
         }),
         paths: {
-            base: process.argv.includes('dev') ? '' : '/deepfakes1m.github.io'
-        }
-    }
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
 
 export default config;
