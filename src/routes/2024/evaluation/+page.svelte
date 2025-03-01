fffb5bc13ab5b56ef62d60333b17e11ef19e277e
ControlNet <smczx@hotmail.com>
2025-03-02 00:49:31 +1100

    Fix lagacy links

--- src/routes/2024/evaluation/+page.svelte
+++ src/routes/2024/evaluation/+page.svelte
@@ -36,17 +36,17 @@
                     <h2 class="card-title">Submission Steps</h2>
                     <h4 class="font-bold">Step 1 - Benchmark platform:</h4>
                     <p>Sign up your account in Codabench and participate the benchmark (please note all participants should submit the form and send us the signed
-                        EULA in <a href="{base}/registration" class="link link-info"
+                        EULA in <a href="{base}/2024/registration" class="link link-info"
                             on:click={() => {
                             tabNum.set(1)
-                            goto('/registration')
+                            goto('/2024/registration')
                         }}>registration</a> to get the dataset).</p>
 
                     <br>
                     <h4 class="font-bold">Step 2 - Prepare predictions:</h4>
-                    <p>Prepare your submission files with the format mentioned in <a href="{base}/details" class="link link-info" on:click={() => {
+                    <p>Prepare your submission files with the format mentioned in <a href="{base}/2024/details" class="link link-info" on:click={() => {
                             tabNum.set(2)
-                            goto('/details')
+                            goto('/2024/details')
                         }}>details</a>. Name your prediction files for <span class="italic">Task 1</span> as "prediction.txt",
                         prediction file for <span class="italic">Task 2</span> as "prediction.json". You can include either or both
                         of them based on the tasks you participate, <span class="font-bold text-primary">then compress them as a zip file</span>.
