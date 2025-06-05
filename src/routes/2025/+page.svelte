95dd3b1e9ee597ebd69f13baf667fb8e1918d556
ControlNet <smczx@hotmail.com>
2025-06-05 16:52:04 +1000

    Revert "Update +page.svelte"
    This reverts commit ed9a06dee4e1d81f9aa0f930828b179dcbce3e80.

--- src/routes/2025/+page.svelte
+++ src/routes/2025/+page.svelte
@@ -49,9 +49,6 @@
                 >Register
                 </button>
             </div>
-        <p class="text"> CMT ACKNOWLEDGMENT: The Microsoft CMT service was used for managing the peer-reviewing process for this conference. 
-This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software 
-development and support. </p>
         </div>
     </div>
     <div class="flex-1"/>
