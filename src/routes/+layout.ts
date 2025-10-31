f379ccea17723bcbc61c40f088ff1459b6b3047e
ControlNet <smczx@hotmail.com>
2025-10-31 16:04:59 +1100

    add trailingSlash for middle routes

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,8 @@
 import { redirect } from '@sveltejs/kit';
 import type { LayoutLoad } from './$types';
 
+export const prerender = true;
+
 export const load: LayoutLoad = async ({ url }) => {
     const pathname = url.pathname;
     
